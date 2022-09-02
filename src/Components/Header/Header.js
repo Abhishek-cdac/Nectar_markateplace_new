f76426b98e9991eebb4892cab7fdd682c051e4de
MrNc18 <karrinagasai12.com>
2022-09-02 12:33:47 +0530

    Changes in landing pages by Nagasai

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,10 +5,21 @@ function Header() {
     <div className="top_header">
       <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-          <img className="header_logo" src="assets/images/Multilogo.jpg" />
-          <a class="navbar-brand" href="#">
-            Market Place
-          </a>
+          <div className="Logo">
+            <img className="header_logo" src="assets/images/60-X-60.png" />
+            <div className="verticalLine_header"
+              style={{
+                borderRight: "1px solid black",
+                height: "25px",
+                marginRight: "5%",
+                marginTop:"5%",
+                marginLeft: "5%",
+              }}
+            ></div>
+            <a class="navbar-brand" href="#">
+              Market Place
+            </a>
+          </div>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
@@ -22,7 +33,7 @@ function Header() {
                 </a>
               </li>
               <li class="nav-item nav_register">
-                <button class="btn-register"  href="#">
+                <button class="btn-register" href="#">
                   Register
                 </button>
               </li>
--- src/Components/Header/Toggle.js
+++ src/Components/Header/Toggle.js
@@ -14,30 +14,42 @@ const Toggle = () => {
         <div className="Faq_heading">
           <h2>Frequently Asked Questions</h2>
         </div>
-        <div style={{marginLeft:"65px",marginTop:"10px"}}>
-        <input
-          checked={value}
-          onChange={handleToggle}
-          className="react-switch-checkbox"
-          id={`react-switch-new`}
-          type="checkbox"
-        />
-        <label
-          className="react-switch-label"
-          style={{ content: (value && "#Reseller") || "Partner" }}
-          htmlFor={`react-switch-new`}
-        >
-          <span className={`react-switch-button`} />
-        </label>
-        {!value ? (
-          <div>
-            <h1>Reseller</h1>
-          </div>
-        ) : (
-          <div>
-            <h1>Partner</h1>
-          </div>
-        )}
+        <div style={{ marginLeft: "65px", marginTop: "10px" }}>
+          <input
+            checked={value}
+            onChange={handleToggle}
+            className="react-switch-checkbox"
+            id={`react-switch-new`}
+            type="checkbox"
+          />
+          <label className="react-switch-label" htmlFor={`react-switch-new`}>
+            {" "}
+            {/* {!value ? ( */}
+            <div className="toggleLabel">
+              <h4>Reseller</h4>
+            </div>
+            {/* ) : ( */}
+            <div className="toggleLabel">
+              <h4>Partner</h4>
+            </div>
+            {/* )} */}
+            <span className={`react-switch-button`}>
+              {/* <h5 className="toggleSwitchLabel">Reseller</h5> */}
+
+              <div className="toggleSwitchLabel">
+                {!value ? <h4>Reseller</h4> : <h4>Partner</h4>}
+              </div>
+            </span>
+          </label>
+          {/* {!value ? (
+            <div>
+              <h1>Reseller</h1>
+            </div>
+          ) : (
+            <div>
+              <h1>Partner</h1>
+            </div>
+          )} */}
         </div>
       </>
     </div>
