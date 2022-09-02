81f68c144bcde481c13676f17b00ee1c001b000e
MrNc18 <karrinagasai12.com>
2022-09-02 09:55:04 +0530

    Home page created new componnets by  Nagasai

--- src/Components/Header/Toggle.js
+++ src/Components/Header/Toggle.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function Toggle() {
+const Toggle = () => {
+  const [value, setValue] = useState(false);
+  const handleToggle = () => {
+    setValue(!value);
+    console.log("togglevalue", value);
+  };
+
   return (
-    <div>Toggle</div>
-  )
-}
+    <div className="Toggle_main">
+      {" "}
+      <>
+        <div className="Faq_heading">
+          <h2>Frequently Asked Questions</h2>
+        </div>
+        <div style={{marginLeft:"65px",marginTop:"10px"}}>
+        <input
+          checked={value}
+          onChange={handleToggle}
+          className="react-switch-checkbox"
+          id={`react-switch-new`}
+          type="checkbox"
+        />
+        <label
+          className="react-switch-label"
+          style={{ content: (value && "#Reseller") || "Partner" }}
+          htmlFor={`react-switch-new`}
+        >
+          <span className={`react-switch-button`} />
+        </label>
+        {!value ? (
+          <div>
+            <h1>Reseller</h1>
+          </div>
+        ) : (
+          <div>
+            <h1>Partner</h1>
+          </div>
+        )}
+        </div>
+      </>
+    </div>
+  );
+};
 
-export default Toggle
+export default Toggle;
--- src/Components/Header/card2.js
+++ src/Components/Header/card2.js
@@ -4,10 +4,10 @@ import dummyData from "../../containers/Dummydata";
 function card2() {
   console.log("dummydata", dummyData);
   return (
-    <div className="row">
+    <div className="row" style={{justifyContent:"center"}}>
       {dummyData &&
         dummyData.CardData.map((data) => (
-          <div className="card  small_card col-12 col-sm-6 col-md-4">
+          <div className="card  small_card col-12 col-sm-6 col-md-3">
             <img src={data.image} style={{height:"61px",width:"81px",paddingTop:"20px"}} alt="..." />
             <div class="card-body">
               <h5 class="card-title">{data.title}</h5>
