58551e4a1f6911d7a17f5ff62fea00533525e303
barkha sharma <barkha.sharma55722@gmail.com>
2022-09-02 12:25:21 +0530

    accordian-becamePartner-code

---
+++ src/Components/Header/Accordian.js
@@ -0,0 +1,181 @@
+import React from 'react'
+
+const Accordian = () => {
+  return (
+    <>
+    <div className="container">
+    
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingOne">
+            <button
+              className="accordion-button  accordian_heading"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+            >
+              What is Nectar Marketplace?
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body accordian_description ">
+              Nectar Marketplace is an online store where nectar users can
+              find and install over 100 ready to use extension and custom apps
+              that expand the core functionality of their Nectat application.
+              Find out more about the Nectar Marketplace here.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingTwo">
+            <button
+              className="accordion-button collapsed  accordian_heading"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              What are extension?
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body accordian_description">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Blanditiis aliquid illo amet. Fugit sapiente molestias rerum?
+              Laudantium saepe animi architecto, nulla tenetur aperiam
+              explicabo sunt laboriosam reprehenderit, culpa suscipit unde!
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingThree">
+            <button
+              className="accordion-button collapsed  accordian_heading"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+              Are extension are safe to use?
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body accordian_description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis quod eaque modi consectetur provident ratione id
+              qui dolorem reprehenderit eius ea molestiae voluptate nemo dicta
+              odit inventore pariatur, quisquam dolore.
+            </div>
+          </div>
+        </div>
+
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingFour">
+            <button
+              className="accordion-button collapsed   accordian_heading"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseFour"
+              aria-expanded="false"
+              aria-controls="collapseFour"
+            >
+              What are different types of solution i can find on Marketplace?
+            </button>
+          </h2>
+          <div
+            id="collapseFour"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingFour"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body accordian_description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+              ad harum placeat alias consequatur odit corrupti et non, omnis
+              perspiciatis corporis ipsum beatae officiis distinctio. Amet at
+              natus corrupti accusamus.
+            </div>
+          </div>
+        </div>
+
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingFive">
+            <button
+              className="accordion-button collapsed accordian_heading"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseFive"
+              aria-expanded="false"
+              aria-controls="collapseFive"
+            >
+              Will anyone in my organization be able ti install or unistall an
+              extension?
+            </button>
+          </h2>
+          <div
+            id="collapseFive"
+            className="accordion-collapse collapse "
+            aria-labelledby="headingFive"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body accordian_description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Blanditiis nostrum possimus iure sit, dicta assumenda, velit
+              nobis voluptas ad corrupti neque accusantium non. Vel,
+              voluptatum. Perspiciatis accusantium est voluptatem minima!
+            </div>
+          </div>
+        </div>
+
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingSix">
+            <button
+              className="accordion-button collapsed accordian_heading"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseSix"
+              aria-expanded="false"
+              aria-controls="collapseSix"
+            >
+              What are the benefit of using marketplace extension?
+            </button>
+          </h2>
+          <div
+            id="collapseSix"
+            className="accordion-collapse collapse "
+            aria-labelledby="headingSix"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body accordian_description ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+              vitae ipsa sapiente fugit eaque. Aperiam quidem asperiores
+              ratione, consequatur vel officiis dolores laborum corporis
+              voluptatibus, ad facere maxime eaque maiores!
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+  )
+}
+
+export default Accordian
--- src/Components/Header/Frequently.js
+++ src/Components/Header/Frequently.js
@@ -1,18 +1,31 @@
 import React from "react";
+import Accordian from "./Accordian";
 import Toggle from "./Toggle";
 
 function Frequently() {
   return (
     <div>
-      <section id="faq" style={{height:"420px"}}>
-        <div className="container" style={{display:"flex",justifyContent:"center"}}>
+      <section id="faq" style={{ height: "240px" }}>
+        <div
+          className="container"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
           <div className="row">
-            <div className="Col-12">
+            <div className="col-12 col-md-12 col-sm-6  col-xs-3">
               <Toggle />
             </div>
           </div>
         </div>
+       
       </section>
+      <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-12 col-sm-6  col-xs-3 ">
+              <Accordian />
+              <br/>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
