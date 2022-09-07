a7c8fdea16c608ec4da3d1bfc51aecb007cab0cf
barkha sharma <barkha.sharma55722@gmail.com>
2022-09-07 11:24:37 +0530

    nectarCRM-page-image-add

--- src/containers/NectarCRMoverview.js
+++ src/containers/NectarCRMoverview.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DummyData from "./DummyNectarCRM";
+import Contact from "../Assets/Contact.svg";
 
 function NectarCRMoverview() {
   console.log("dummydata", DummyData);
@@ -31,48 +32,55 @@ function NectarCRMoverview() {
               <p> {data.head5}</p>
             </div>
             <div class="midsec pt-5">
-      <div class="container ">
-        <div class="midimg ">
-            <img src="" alt=""/>
-        </div>
-   <div class="midform ">
-    <h2>We Would Love To Hear From You!</h2>
-        <div class="border-bottom">
-          <label for="exampleInputEmail1">Full Name</label> 
-        </div>
-        <div class="border-bottom">
-          <label for="exampleInputPassword1">Email ID</label>
-           </div>
-           <div class="border-bottom">
-            <label for="exampleInputPassword1">Password</label>
-             </div>
-             <div class="border-bottom">
-              <label for="exampleInputPassword1">Contact Number</label>
-               </div>
-               <div>
-                <p class="midpara">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis expedita fugit quae quo minima aliquam. Veniam doloremque nesciunt distinctio asperiores pariatur.</p>
-               </div>
-              
-        <div class="form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">I agree to the Terms of Service, Marketplace Terms of Use and Privacy Policy.</label>
-        </div>
-        <br />
-        <br/>
-        <button type="submit" class="btn  float-right midbutton">Submit</button>
-   
-   </div>
-    </div>
-    </div>
-          </div>
+              <div class="container ">
+                <div class="midimg ">
+                  <img src={Contact} alt="" height="500px" />
+                </div>
+                <div class="midform p-5"  >
+                  <h3 style={{color:"#08668F"}}>We Would Love To Hear From You!</h3>
+                  <div class="border-bottom" style={{fontSize:14}}>
+                    <label for="exampleInputEmail1">Full Name</label>
+                  </div>
+                  <div class="border-bottom" style={{fontSize:14}}>
+                    <label for="exampleInputPassword1">Email ID</label>
+                  </div>
+                  <div class="border-bottom" style={{fontSize:14}}>
+                    <label for="exampleInputPassword1">Password</label>
+                  </div>
+                  <div class="border-bottom" style={{fontSize:14}}>
+                    <label for="exampleInputPassword1">Contact Number</label>
+                  </div>
+                  <div>
+                    <p class="midpara"  >
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Omnis expedita fugit quae quo minima aliquam. Veniam
+                      doloremque nesciunt distinctio asperiores pariatur.
+                    </p>
+                  </div>
 
+                  <div class="form-check">
+                    <input
+                      type="checkbox"
+                      class="form-check-input"
+                      id="exampleCheck1"
+                    />
+                    <label class="form-check-label" for="exampleCheck1">
+                      I agree to the Terms of Service, Marketplace Terms of Use
+                      and Privacy Policy.
+                    </label>
+                  </div>
+                  <br />
+                  <br />
+                  <button type="submit" class="btn  float-right midbutton">
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         ))}
-    
     </div>
-    
   );
 }
 
 export default NectarCRMoverview;
-
- 
