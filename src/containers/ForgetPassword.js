3d89fc288edb90e56c75b91bfa6a0b8523876e76
priyanka211999 <priyanka.satpute@nectarinfotel.com>
2022-12-14 14:26:32 +0530

    forgate password

--- src/containers/ForgetPassword.js
+++ src/containers/ForgetPassword.js
@@ -1,47 +1,48 @@
 import React, { useState } from "react";
 
 function ForgetPassword() {
-
   return (
     <>
       <section id="loginbg1">
         <div className="container">
           <div className="row">
             <div className="col-12 col-lg-12">
-                <div class="panel panel-default">
-                  <div class="panel-body">
-                    <div class="text-center">
-                      <h3>
-                        <i class="fa fa-lock fa-4x"></i>
-                      </h3>
-                      <h2 class="text-center">Forgot Password?</h2>
-                      <div class="panel-body1">
-                        <form
-                          id="register-form"
-                          role="form"
-                          autocomplete="off"
-                          class="form"
-                          method="post"
-                        >
-                          <div class="form-group">
-                            <div class="input-group">
-                              <input
-                                id="email"
-                                name="email"
-                                placeholder="Enter your Email"
-                                class="form-control"
-                                type="email"
-                              />
-                            </div>
+              <div class="panel panel-default">
+                <div class="panel-body">
+                  <div class="text-center">
+                    <h3>
+                      <i class="fa fa-lock fa-4x"></i>
+                    </h3>
+                    <h2 class="text-center">Forgot Password?</h2>
+                    <div class="panel-body1">
+                      <form
+                        id="register-form"
+                        role="form"
+                        autocomplete="off"
+                        class="form"
+                        method="post"
+                      >
+                        <div class="form-group">
+                          <div class="input-group">
+                            <input
+                              id="email"
+                              name="email"
+                              placeholder="Enter your Email"
+                              class="form-control"
+                              type="email"
+                            />
                           </div>
-                          <div class="b1">
-                            <button className="resetbutton">Reset Password</button>
-                          </div>
-                        </form>
-                      </div>
+                        </div>
+                        <div class="b1">
+                          <button className="resetbutton">
+                            Reset Password
+                          </button>
+                        </div>
+                      </form>
                     </div>
                   </div>
                 </div>
+              </div>
             </div>
           </div>
         </div>
